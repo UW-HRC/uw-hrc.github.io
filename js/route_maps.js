8266d302df4be9b7103fd200ccfe91e28e0128c1
Sam Wolfson <wolfson@cs.uw.edu>
2016-11-17 22:42:36 -0800

    more map embed updates

--- js/route_maps.js
+++ js/route_maps.js
@@ -1,28 +1,28 @@
 var routes = {
   'interlaken': {
-    3: '//snippets.mapmycdn.com/routes/view/embedded/356639835?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00',
-    5: '//snippets.mapmycdn.com/routes/view/embedded/380810830?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00',
-    7: '//snippets.mapmycdn.com/routes/view/embedded/380811260?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00'
+    3: '//snippets.mapmycdn.com/routes/view/embedded/356639835?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    5: '//snippets.mapmycdn.com/routes/view/embedded/380810830?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    7: '//snippets.mapmycdn.com/routes/view/embedded/380811260?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
   },
   'arboretum': {
-    3: '//snippets.mapmycdn.com/routes/view/embedded/380811542?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00',
-    5: '//snippets.mapmycdn.com/routes/view/embedded/380811742?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00',
-    7: '//snippets.mapmycdn.com/routes/view/embedded/380812136?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00'
+    3: '//snippets.mapmycdn.com/routes/view/embedded/380811542?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    5: '//snippets.mapmycdn.com/routes/view/embedded/380811742?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    7: '//snippets.mapmycdn.com/routes/view/embedded/380812136?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
   },
   'ravenna': {
-    3: '//snippets.mapmycdn.com/routes/view/embedded/1362222805?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2016-11-17T22:15:40-08:00',
-    5: '//snippets.mapmycdn.com/routes/view/embedded/381208242?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
-    7: '//snippets.mapmycdn.com/routes/view/embedded/381213640?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
+    3: '//snippets.mapmycdn.com/routes/view/embedded/1362222805?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2016-11-17T22:15:40-08:00',
+    5: '//snippets.mapmycdn.com/routes/view/embedded/381208242?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    7: '//snippets.mapmycdn.com/routes/view/embedded/381213640?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
   },
   'laurelhurst': {
-    3: '//snippets.mapmycdn.com/routes/view/embedded/380812480?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00%22%20height=%22400px%22%20width=%22100%%22%20frameborder=%220%22',
-    5: '//snippets.mapmycdn.com/routes/view/embedded/380812908?width=600&amp;height=400&amp;&amp;line_color=E60f0bdb&amp;rgbhex=DB0B0E&amp;distance_markers=0&amp;unit_type=imperial&amp;map_mode=ROADMAP&amp;last_updated=2014-03-29T17:09:05-05:00',
-    7: '//snippets.mapmycdn.com/routes/view/embedded/380813832?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
+    3: '//snippets.mapmycdn.com/routes/view/embedded/380812480?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00%22%20height=%22400px%22%20width=%22100%%22%20frameborder=%220%22',
+    5: '//snippets.mapmycdn.com/routes/view/embedded/380812908?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    7: '//snippets.mapmycdn.com/routes/view/embedded/380813832?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
   },
   'green_lake': {
-    3: '//snippets.mapmycdn.com/routes/view/embedded/381215294?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
-    5: '//snippets.mapmycdn.com/routes/view/embedded/381216728?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
-    7: '//snippets.mapmycdn.com/routes/view/embedded/381218394?width=600&height=400&&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
+    3: '//snippets.mapmycdn.com/routes/view/embedded/381215294?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    5: '//snippets.mapmycdn.com/routes/view/embedded/381216728?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00',
+    7: '//snippets.mapmycdn.com/routes/view/embedded/381218394?width=600&height=400&line_color=E60f0bdb&rgbhex=DB0B0E&distance_markers=0&unit_type=imperial&map_mode=ROADMAP&last_updated=2014-03-29T17:09:05-05:00'
   }
 }
 
@@ -60,6 +60,6 @@ function updateRoutes() {
 
   if(name !== "" && !isNaN(distance)) {
     var route = routes[name][distance];
-    document.getElementById("route-container").innerHTML = '<iframe id="mapmyfitness_route" src="' + route + '" height="400px" width="100%" frameborder="0"></iframe><div style="text-align: right; padding-right: 20px;">';
+    document.getElementById("route-container").innerHTML = '<iframe id="mapmyfitness_route" src="' + route + '" height="400px" width="100%" frameborder="0"></iframe>';
   }
 }
